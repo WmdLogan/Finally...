601130d0d98ff5dc6507b920cf325192cf2c0443
WmdLogan <710222440@qq.com>
2020-06-25 11:27:31 +0800

    Leetcode Begin

--- DataStructure/206.cpp
+++ DataStructure/206.cpp
@@ -2,6 +2,7 @@
 // 206. Reverse Linked List(Easy)
 // 83. Remove Duplicates from Sorted List(Easy)
 // 19. Remove Nth Node From End of List(Medium)
+// 24. Swap Nodes in Pairs
 
 /**
  * Definition for singly-linked list.*/
@@ -51,8 +52,7 @@ public:
         }
         ListNode *h = head;
         ListNode *end = head;
-        while ( end != nullptr )
-        {
+        while (end != nullptr) {
             if (n >= 0) {
                 end = end->next;
                 n--;
@@ -67,14 +67,36 @@ public:
         h->next = h->next->next;
         return head;
     }
+
+    ListNode *swapPairs(ListNode *head) {
+        if (head == nullptr || head->next == nullptr) {
+            return head;
+        }
+        ListNode *pre = head;
+        ListNode *cur = head->next;
+        pre->next = cur->next;
+        cur->next = pre;
+        head = cur;
+        while (pre->next != nullptr) {
+            cur = pre->next;
+            if (cur->next == nullptr) {
+                break;
+            }
+            pre->next = pre->next->next;
+            cur->next = cur->next->next;
+            pre->next->next = cur;
+            pre = cur;
+        }
+        return head;
+    }
 };
 
 int main() {
     ListNode a1(1);
-    ListNode a2(1);
-    ListNode c1(2);
-    ListNode c2(3);
-    ListNode c3(3);
+    ListNode a2(2);
+    ListNode c1(3);
+    ListNode c2(4);
+    ListNode c3(5);
     ListNode b1(5);
     ListNode b2(0);
     ListNode b3(1);
@@ -87,7 +109,8 @@ int main() {
     b2.next = &b3;
     b3.next = &c1;
     Solution a;
-    ListNode *b = a.reverseList(&a1);
+    ListNode *b = a.swapPairs(&a1);
+
     while (b != NULL) {
         cout << b->val << " ";
         b = b->next;
