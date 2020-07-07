f665fbda7f292c6966bde6c9755c24dc72686872
WmdLogan <710222440@qq.com>
2020-07-07 17:49:23 +0800

    Leetcode Begin

--- DataStructure/Tree.cpp
+++ DataStructure/Tree.cpp
@@ -2,6 +2,8 @@
 // Created by logan on 2020/7/6.
 //104. Maximum Depth of Binary Tree (Easy)
 // 110. Balanced Binary Tree (Easy)
+// 543. Diameter of Binary Tree (Easy)
+// 226. Invert Binary Tree (Easy)
 #include <iostream>
 #include <deque>
 
@@ -35,12 +37,49 @@ public:
         return deep;
     }
 
-    bool isBalanced(TreeNode* root) {
-        if(!root) return true;
+    bool isBalanced(TreeNode *root) {
+        if (!root) return true;
 
-        int d = abs(maxDepth(root->left)-maxDepth(root->right)); //当前节点的左右子树的高度差
+        int d = abs(maxDepth(root->left) - maxDepth(root->right)); //当前节点的左右子树的高度差
 
-        return (d<=1) && (isBalanced(root->left)) && (isBalanced(root->right));
+        return (d <= 1) && (isBalanced(root->left)) && (isBalanced(root->right));
     }
 
+public:
+    int Max;
+
+    int depth(TreeNode *rt) {
+        if (rt == nullptr) {
+            return 0;
+        }
+        int L, R;
+        L = depth(rt->left);
+        R = depth(rt->right);
+        if (L + R + 1 > Max) {
+            Max = L + R + 1;
+        }
+        return max(L, R) + 1;
+    }
+
+    int diameterOfBinaryTree(TreeNode *root) {
+        Max = 1;
+        depth(root);
+        return Max - 1;
+    }
+
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if (root == nullptr) {
+            return nullptr;
+        }
+        TreeNode *temp;
+        if (root->right || root->left) {
+            temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        return root;
+    }
 };
