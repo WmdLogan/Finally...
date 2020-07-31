4046cfbf765ecc76c586d331d9abd593d86dc5ce
WmdLogan <710222440@qq.com>
2020-07-31 15:27:39 +0800

    update

--- DataStructure/Bit.cpp
+++ DataStructure/Bit.cpp
@@ -5,9 +5,12 @@
 // 136. Single Number (Easy)
 // 268. Missing Number (Easy)
 // 260. Single Number III (Medium)
+// 190. Reverse Bits (Easy)
 
 #include <iostream>
 #include <vector>
+#include <map>
+
 using namespace std;
 
 class Solution {
@@ -52,4 +55,27 @@ public:
         return ans;
     }
 // 260. Single Number III (Medium)
+    vector<int> singleNumber_t(vector<int>& nums) {
+        int ans = 0;
+        for (auto e :nums) {
+            ans ^= e;
+        }
+        ans &= -ans;
+        vector<int> re(2, 0);
+        for (auto e :nums) {
+           if ( (e & ans) == 0) re[0] ^= e;
+           else re[1] ^= e;
+        }
+        return re;
+    }
+// 190. Reverse Bits (Easy)
+    uint32_t reverseBits(uint32_t n) {
+        n = (n >> 16) | (n << 16);
+        n = ((n & 0xff00ff00) >> 8) | ((n & 0x00ff00ff) << 8);
+        n = ((n & 0xf0f0f0f0) >> 4) | ((n & 0x0f0f0f0f) << 4);
+        n = ((n & 0xcccccccc) >> 2) | ((n & 0x33333333) << 2);
+        n = ((n & 0xaaaaaaaa) >> 1) | ((n & 0x55555555) << 1);
+        return n;
+    }
+
 };
