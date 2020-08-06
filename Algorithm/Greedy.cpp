086da93dae3b460254caa9c013065bb491ad1329
WmdLogan <710222440@qq.com>
2020-08-06 18:39:06 +0800

    update

--- Algorithm/Greedy.cpp
+++ Algorithm/Greedy.cpp
@@ -3,8 +3,17 @@
 //
 // 455. Assign Cookies (Easy)
 // 435. Non-overlapping Intervals (Medium)
+// 452. Minimum Number of Arrows to Burst Balloons (Medium)
+// 406. Queue Reconstruction by Height(Medium)
+// 121. Best Time to Buy and Sell Stock (Easy)
+// 122. Best Time to Buy and Sell Stock II (Easy)
+// 605. Can Place Flowers (Easy)
+// 392. Is Subsequence (Medium)
+// 665. Non-decreasing Array (Easy)
 #include <vector>
 #include <algorithm>
+#include <list>
+#include <iostream>
 
 using namespace std;
 
@@ -50,5 +59,130 @@ public:
         }
         return size - ans;
     }
+
+// 452. Minimum Number of Arrows to Burst Balloons (Medium)
+    static bool compare_balloon(vector<int> &a, vector<int> &b) {
+        return a[0] < b[0];
+    }
+
+    int findMinArrowShots(vector<vector<int>> &points) {
+        if (points.empty()) return 0;
+        int ans = 1;
+        sort(points.begin(), points.end(), compare_balloon);
+        int right = points[0][1];
+        for (auto m:points) {
+            if (m[1] <= right) {
+                //左右点都在边界中,更新右边界
+                right = m[1];
+                //左点在右点不在，右边界不变
+            }
+                //左右点都不在
+            else if (m[0] > right) {
+                ans++;
+                right = m[1];
+            }
+        }
+        return ans;
+    }
+
+// 406. Queue Reconstruction by Height(Medium)
+    vector<vector<int>> reconstructQueue(vector<vector<int>> &people) {
+        sort(people.begin(), people.end(), [](vector<int> &a, vector<int> &b) {
+            return a[0] == b[0] ? a[1] < b[1] : a[0] > b[0];
+        });
+        list<vector<int>> ans;
+        list<vector<int>>::iterator loc;
+        for (auto m:people) {
+            loc = ans.begin();
+            advance(loc, m[1]);
+            ans.insert(loc, m);
+        }
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+
+// 121. Best Time to Buy and Sell Stock (Easy)
+    int maxProfit(vector<int> &prices) {
+        if (prices.empty()) return 0;
+        int min_price = prices[0], max_profit = 0;
+        for (int price: prices) {
+            max_profit = max(max_profit, price - min_price);
+            min_price = min(price, min_price);
+        }
+        return max_profit;
+    }
+
+// 122. Best Time to Buy and Sell Stock II (Easy)
+    int maxProfit_2(vector<int> &prices) {
+        int len = prices.size();
+        if (len == 0) return 0;
+        int ans = 0;
+        int single_profit = prices[0];
+        for (int i = 0; i < len - 1; ++i) {
+            if (prices[i] >= prices[i + 1]) {//下一天价格大于今天价格，卖出，买新的
+                ans += (prices[i] - single_profit);
+                single_profit = prices[i + 1];
+            }
+        }
+        return prices[len - 1] > single_profit ? ans + prices[len - 1] - single_profit : ans;
+    }
+
+// 605. Can Place Flowers (Easy)
+    bool canPlaceFlowers(vector<int> &flowerbed, int n) {
+        int len = flowerbed.size();
+        int i = 0;
+        int left = 0;//记录当前位置左边是否有花
+        for (; i < len - 1; i++) {
+            if (flowerbed[i] == 0 && left == 0 && flowerbed[i + 1] == 0) {
+                //当前位置空,左边空,右边空，种花
+                n--;
+                if (n <= 0) return true;
+                left = 1;
+            } else left = flowerbed[i];
+        }
+        if (n == 0 || n == 1 && left == 0 && flowerbed[i] == 0) return true;
+        return false;
+    }
+
+// 392. Is Subsequence (Medium)
+    bool isSubsequence(string s, string t) {
+        int s_len = s.length();
+        int t_len = t.length();
+        int i = 0, j = 0;
+        while (i < s_len && j < t_len) {
+            if (s[i] == t[j]) {
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return i == s_len;
+    }
+
+// 665. Non-decreasing Array (Easy)
+    bool checkPossibility(vector<int> &nums) {
+        int count = 0;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            if (nums[i] > nums[i + 1]) {
+                if (i == 0 || nums[i - 1] <= nums[i + 1]) {//第一个结点不满足
+                    count++;
+                    if (count == 2) return false;
+                } else {
+                    //其他情况，下一个结点等于当前结点
+                    count++;
+                    nums[i + 1] = nums[i];
+                    if (count == 2) return false;
+                }
+            }
+        }
+        return true;
+    }
+
+
 };
 
+int main() {
+    vector<int> a = {4, 2, 3};
+    Solution solution;
+    cout << solution.checkPossibility(a);
+}
