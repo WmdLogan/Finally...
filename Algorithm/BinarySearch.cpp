790a3a46ec17338c31aa802630b733dcee885d3f
WmdLogan <710222440@qq.com>
2020-08-09 18:40:52 +0800

    update

--- Algorithm/BinarySearch.cpp
+++ Algorithm/BinarySearch.cpp
@@ -3,8 +3,14 @@
 //
 // 69. Sqrt(x) (Easy)
 // 744. Find Smallest Letter Greater Than Target (Easy)
+// 540. Single Element in a Sorted Array (Medium)
+// 278. First Bad Version (Easy)
+// 153. Find Minimum in Rotated Sorted Array (Medium)
+// 34. Find First and Last Position of Element in Sorted Array (Medium)
 
 #include <vector>
+#include <iostream>
+
 
 using namespace std;
 
@@ -35,10 +41,87 @@ public:
             if (letters[mid] > target) {
                 right = mid;
             } else {
+                //小于等于key的都舍弃
                 left = mid + 1;
             }
         }
-        if (left == letters.size()) return letters[0];
-        return letters[left];
+        return letters[left % letters.size()];
+    }
+
+// 540. Single Element in a Sorted Array (Medium)
+    int singleNonDuplicate(vector<int> &nums) {
+        int r = nums.size() - 1;
+        int l = 0;
+        while (r > l) {//将查找范围缩小到3个数
+            int mid = l + (r - l) / 2;
+            if (mid % 2 == 1) {//mid为奇数
+                if (nums[mid - 1] == nums[mid]) l = mid + 1;
+                else r = mid - 1;
+            } else {//mid为偶数
+                if (nums[mid - 1] == nums[mid]) r = mid;
+                else l = mid;
+            }
+        }
+        return nums[l];
+    }
+
+// 278. First Bad Version (Easy)
+    bool isBadVersion(int version) { return false; }
+
+    int firstBadVersion(int n) {
+        int l = 1;
+        while (l < n) {
+            int m = l + (n - l) / 2;
+            if (isBadVersion(m)) {
+                n = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+
+// 153. Find Minimum in Rotated Sorted Array (Medium)
+    int findMin(vector<int> &nums) {
+        int l = 0, h = nums.size() - 1;
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] <= nums[h]) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return nums[l];
+    }
+
+// 34. Find First and Last Position of Element in Sorted Array (Medium)
+    vector<int> searchRange(vector<int> &nums, int target) {
+        vector<int> ans(2, -1);
+        int first = findFirst(nums, target);
+        if (first == nums.size() || nums[first] != target) return ans;//当key比最后一个元素大，返回值越界
+        ans[0] = first;
+        ans[1] = findFirst(nums, target + 1) - 1;
+        return ans;
+    }
+
+    int findFirst(vector<int> &nums, int target) {
+        int l = 0, h = nums.size(); // h增大，这样可以返回末尾值
+        while (l < h) {
+            int m = l + (h - l) / 2;
+            if (nums[m] >= target) {
+                h = m;
+            } else {
+                l = m + 1;
+            }
+        }
+        return l;
     }
 };
+
+int main() {
+    Solution solution;
+    vector<int> vec = {2, 2};
+    cout << solution.singleNonDuplicate(vec);
+    return 0;
+}
