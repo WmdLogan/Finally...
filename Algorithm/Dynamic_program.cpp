dc4018192b00a65591df112144938301e4ea2d94
WmdLogan <710222440@qq.com>
2020-08-24 22:48:34 +0800

    commit

--- Algorithm/Dynamic_program.cpp
+++ Algorithm/Dynamic_program.cpp
@@ -178,5 +178,22 @@ public:
     }
 
 // 646. Maximum Length of Pair Chain (Medium)
+    int findLongestChain_1(vector<vector<int>> &pairs) {
+        if (pairs.empty())return 0;
+        sort(pairs.begin(), pairs.end(), [](const vector<int> &a, const vector<int> &b) {
+            return (a[0] == b[0] && a[1] < b[1]) || (a[0] < b[0]);
+        });
+        int n = pairs.size(), res = 0;
+        vector<int> dp(n, 1);
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = max(dp[i], dp[j] + 1);
+                }
+            }
+            res = max(res, dp[i]);
+        }
+        return res;
+    }
 
 };
