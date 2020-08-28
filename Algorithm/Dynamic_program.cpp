63ecfccd47e9a1afb5027809e9c7e368cffc64e9
WmdLogan <710222440@qq.com>
2020-08-28 16:48:07 +0800

    commit

--- Algorithm/Dynamic_program.cpp
+++ Algorithm/Dynamic_program.cpp
@@ -17,6 +17,7 @@
 // 1143. Longest Common Subsequence
 // 416. Partition Equal Subset Sum (Medium)
 // 494. Target Sum (Medium)
+// 474. Ones and Zeroes (Medium)
 
 #include <iostream>
 #include <vector>
@@ -344,7 +345,7 @@ public:
         for (int i:nums) {
             sum += i;
         }
-        // 背包容量为整数，sum+S为奇数的话不满足要求
+        // sum+S为奇数的话不满足要求
         if ((sum % 2) != (S % 2)) {
             return 0;
         }
@@ -364,6 +365,29 @@ public:
 
         return dp[len];
     }
+
+// 474. Ones and Zeroes (Medium)
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        int S = strs.size();
+        vector<vector<int> > dp(m + 1, vector<int>(n + 1, 0));
+        for (int l = 0; l < S; ++l) {
+            int zero = 0;
+            int one = 0;
+            for (int i = 0; i < strs[l].size(); ++i) {//统计字符串中0和1的个数
+                if (strs[l][i] == '0') ++zero;
+                else ++one;
+            }
+            for (int i = m; i >= zero; --i) {
+                for (int j = n; j >= one; --j) {
+                    dp[i][j] = max(dp[i][j], 1 + dp[i - zero][j - one]);
+                }
+            }
+        }
+        return dp[m][n];
+    }
+
+
+
 };
 int main(){
     vector<int> nums = {1, 1, 1, 1, 1};
