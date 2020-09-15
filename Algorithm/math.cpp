6e4ec1f5aadb3edf72003b87ccf27703a9b206d9
WmdLogan <710222440@qq.com>
2020-09-15 09:05:45 +0800

    commit

--- Algorithm/math.cpp
+++ Algorithm/math.cpp
@@ -15,11 +15,13 @@
 // 367. Valid Perfect Square (Easy)
 // 326. Power of Three (Easy)
 // 238. Product of Array Except Self (Medium)
+// 628. Maximum Product of Three Numbers (Easy)
 #include <iostream>
 #include <vector>
 #include <algorithm>
 #include <algorithm>
 #include <map>
+#include <climits>
 
 using namespace std;
 
@@ -212,6 +214,34 @@ public:
             R *= nums[i];
         }
         return answer;
+    }
+
+// 628. Maximum Product of Three Numbers (Easy)
+    int maximumProduct(vector<int> &nums) {
+//假设max1 > max2 > max3
+        int max1 = INT_MIN, max2 = INT_MIN, max3 = INT_MIN;
+//假设min1 < min2
+        int min1 = INT_MAX, min2 = INT_MAX;
+        for (int a : nums) {
+            if (a > max1) {
+                max3 = max2;
+                max2 = max1;
+                max1 = a;
+            } else if (a > max2) {
+                max3 = max2;
+                max2 = a;
+            } else if (a > max3) {
+                max3 = a;
+            }
+
+            if (a < min1) {
+                min2 = min1;
+                min1 = a;
+            } else if (a < min2) {
+                min2 = a;
+            }
+        }
+        return max(min1 * min2 * max1, max1 * max2 * max3);
     }
 };
 
