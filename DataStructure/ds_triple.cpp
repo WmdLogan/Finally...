1435d75d7e0621899877fb8111b850c196eb1a51
WmdLogan <710222440@qq.com>
2020-10-28 15:40:24 +0800

    commit

--- DataStructure/ds_triple.cpp
+++ DataStructure/ds_triple.cpp
@@ -5,7 +5,8 @@
 #include <queue>
 #include <vector>
 #include <stack>
-
+#include <unordered_map>
+#include <map>
 using namespace std;
 
 struct TreeNode {
@@ -81,4 +82,78 @@ public:
     bool isBalanced(TreeNode *root) {
         return height(root) >= 0;
     }
+
+// 111. Minimum Depth of Binary Tree
+    int minDepth(TreeNode* root) {
+        if (root == nullptr) return 0;
+        int ans_depth = 0;
+        queue<TreeNode*> q;
+        q.emplace(root);
+        while (!q.empty()) {
+            ans_depth++;
+            int size = q.size();
+            while (size > 0 ){
+                size--;
+                auto temp = q.front();
+                q.pop();
+                if (temp->left == nullptr && temp->right == nullptr) return ans_depth;
+                if (temp->left) q.emplace(temp->left);
+                if (temp->right) q.emplace(temp->right);
+            }
+        }
+        return ans_depth;
+    }
+
+// 205. Isomorphic Strings
+    bool isIsomorphic(string s, string t) {
+        int len = s.size();
+        for (int i = 0; i < len; i++) {
+            if (s.find(s[i]) != t.find(t[i]))
+                return false;
+        }
+        return true;
+    }
+
+// 207. Course Schedule
+    bool canFinish(int numCourses, vector<vector<int>>& prerequisites) {
+        vector<int> in_degree(numCourses, 0);//入度表
+        vector<vector<int>> in_list(numCourses);
+        queue<int> class_queue;
+        int flag = 0;
+//构造入度表
+        for (auto prerequisite:prerequisites) {
+            in_degree[prerequisite[0]]++;
+            in_list[prerequisite[1]].emplace_back(prerequisite[0]);
+        }
+//入度为0的课，入队列
+        for (int i = 0; i < numCourses; ++i) {
+            if (in_degree[i] == 0){
+                flag++;
+                class_queue.push(i);
+            }
+        }
+        while (!class_queue.empty()) {
+            int size = class_queue.size();
+            while (size) {
+                int temp = class_queue.front();
+                class_queue.pop();
+                for (int i = 0; i < in_list[temp].size(); ++i) {
+                    in_degree[in_list[temp][i]]--;
+                    if (in_degree[in_list[temp][i]] == 0) {
+                        class_queue.push(in_list[temp][i]);
+                        flag++;
+                    }
+                }
+                size--;
+            }
+        }
+        return flag == numCourses;
+    }
+
+
 };
+int main(){
+    Solution s;
+    vector<vector<int>> vec = {{1, 0}};
+    cout << s.canFinish(2, vec);
+}
