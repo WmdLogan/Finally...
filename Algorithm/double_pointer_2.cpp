54d4270bea1c1064cb39acf277c6366d2d5762f9
WmdLogan <710222440@qq.com>
2020-11-21 17:21:51 +0800

    commit

--- Algorithm/double_pointer_2.cpp
+++ Algorithm/double_pointer_2.cpp
@@ -15,10 +15,17 @@
 
 using namespace std;
 
+struct ListNode {
+    int val;
+    ListNode *next;
+
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
 class Solution {
 public:
 // 167. 两数之和 II - 输入有序数组
-    vector<int> twoSum(vector<int>& numbers, int target) {
+    vector<int> twoSum(vector<int> &numbers, int target) {
         vector<int> ans;
         int i = 0;
         int j = numbers.size() - 1;
@@ -28,8 +35,7 @@ public:
                 ans.push_back(i + 1);
                 ans.push_back(j + 1);
                 return ans;
-            }
-            else if (temp > target) j--;
+            } else if (temp > target) j--;
             else i++;
         }
         return ans;
@@ -38,11 +44,11 @@ public:
 // 633. 平方数之和
     bool judgeSquareSum(int c) {
         long high = sqrt(c), low = 0;
-        while(low <= high) {
+        while (low <= high) {
             long sum = high * high + low * low;
-            if( sum == c )
+            if (sum == c)
                 return true;
-            else if(sum < c)
+            else if (sum < c)
                 low++;
             else
                 high--;
@@ -93,6 +99,54 @@ public:
         }
         if (l2 >= r2) return true;
         return false;
+    }
+
+// 88. 合并两个有序数组
+    void merge(vector<int> &nums1, int m, vector<int> &nums2, int n) {
+        int p = m + n;
+        m--;
+        n--;
+        while (n >= 0 && m >= 0) {
+            nums1[--p] = nums1[m] <= nums2[n] ? nums2[n--] : nums1[m--];
+        }
+        if (m < 0) {
+            while (n >= 0) {
+                nums1[--p] = nums2[n--];
+            }
+        }
+    }
+
+// 141. 环形链表
+    bool hasCycle(ListNode *head) {
+        if (!head) return false;
+        auto fast = head;
+        auto slow = head;
+        while (fast != nullptr && fast->next != nullptr) {
+            fast = fast->next->next;
+            slow = slow->next;
+            if (fast == slow) return true;
+        }
+        return false;
+    }
+
+// 524. 通过删除字母匹配到字典里最长单词
+    string findLongestWord(string s, vector<string> &d) {
+        string ans = "";
+        for (string target:d) {
+            int l1 = ans.length(), l2 = target.length();
+            if (l1 > l2 || (l1 == l2 && ans < target)) continue;
+            if (isStr(s, target)) ans = target;
+        }
+        return ans;
+    }
+
+    bool isStr(string s, string target) {
+        int i = 0, j = 0;
+        while (i < s.length() && j < target.length()) {
+            if (s[i] == target[j]) j++;
+            i++;
+        }
+        return j == target.length();
     }
 };
 
