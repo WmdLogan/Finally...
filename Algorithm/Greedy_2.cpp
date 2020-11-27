f4a11544dfd00367a1fd4fdb460b0c5e3106f612
WmdLogan <710222440@qq.com>
2020-11-27 08:54:31 +0800

    commit

--- Algorithm/Greedy_2.cpp
+++ Algorithm/Greedy_2.cpp
@@ -22,5 +22,44 @@ using namespace std;
 
 class Solution {
 public:
+// 455. 分发饼干
+    int findContentChildren(vector<int> &g, vector<int> &s) {
+        int ans = 0;
+        sort(g.begin(), g.end());
+        sort(s.begin(), s.end());
+        int i = 0, j = 0;
+        int s_len = s.size();
+        int g_len = g.size();
+        while (j < s_len && i < g_len) {
+            if (g[i] <= s[j]) {
+                ans++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
 
+// 435. 无重叠区间
+    int eraseOverlapIntervals(vector<vector<int>> &intervals) {
+        if (intervals.empty()) return 0;
+        int ans = 0;
+        sort(intervals.begin(), intervals.end(), [&](vector<int> &a, vector<int> &b) {
+            if (a[1] == b[1]) return a[0] > b[0];
+            return a[1] < b[1];
+        });
+        int last_right = intervals[0][1];
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (last_right > intervals[i][0]) ans++;
+            else last_right = intervals[i][1];
+        }
+        return ans;
+    }
+
+// 452. 用最少数量的箭引爆气球
+    int findMinArrowShots(vector<vector<int>>& points) {
+
+    }
 };
